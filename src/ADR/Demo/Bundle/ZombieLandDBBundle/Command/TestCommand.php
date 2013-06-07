9054acecca745a9077cb30c5b5e1521e3f7e40ea
marcosQuesada <marcos.quesadas@gmail.com>
2013-06-07 23:36:00 +0200

    added number of process argument on TestCommand

--- src/ADR/Demo/Bundle/ZombieLandDBBundle/Command/TestCommand.php
+++ src/ADR/Demo/Bundle/ZombieLandDBBundle/Command/TestCommand.php
@@ -1,9 +1,9 @@
 <?php
-
 namespace ADR\Demo\Bundle\ZombieLandDBBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputOption;
 
@@ -17,17 +17,19 @@ class TestCommand extends ContainerAwareCommand
             ->setHelp(<<<EOT
 The <info>zldb:test</info> command tests ZombieLandDB.
 EOT
-            );
+            )->addArgument('totalProcesses', InputArgument::OPTIONAL, 'Total process number', 10000);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $totalProcesses = $input->getArgument('totalProcesses');
+
         $node = $this->getContainer()->get('adr_symfony2erlang.channel.manager')->getChannel('peb_node0');
         $pids = array();
 
         // Test 1: Create processes
         $ts0 = microtime(true);
-        for($i=1; $i<=10000; $i++)
+        for($i=1; $i <= $totalProcesses; $i++)
         {
             list($rOk, $pid) = $node->call('zldb_manager', 'get_pid_from_id', array('abc' . $i));
             $pids[$i] = $pid;
@@ -36,7 +38,7 @@ EOT
 
         // Test 2: Set data
         $ts0 = microtime(true);
-        for($i=1; $i<=10000; $i++)
+        for($i=1; $i <= $totalProcesses; $i++)
         {
             $rOk = $node->call('zldb_entity', 'set', array($pids[$i], 'foo', 'data foo'));
         }
@@ -44,7 +46,7 @@ EOT
 
         // Test 3: Get data
         $ts0 = microtime(true);
-        for($i=1; $i<=10000; $i++)
+        for($i=1; $i <= $totalProcesses; $i++)
         {
             $data = $node->call('zldb_entity', 'get', array($pids[$i], 'foo'));
         }
